54cdcaa0f83605a8b6ed137a9e566f852cdfc93d
bigfishh <anniezheng.anniee@gmail.com>
2020-10-13 17:04:50 -0400

    mock cc review

--- src/components/GreatHall.jsx
+++ src/components/GreatHall.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Wizard from './Wizard'
 
-const GreatHall = () => {
+const GreatHall = (props) => {
+  // access the props 
+  // map over the array of wizards 
+  // pull out the specific keys that I want to use in the render of the wizard -- passing down as props
+  let wizards = props.wizards.map((wizard) => {
+    // <Wizard key={wizard.id} {...wizard} />
+    return <Wizard key={wizard.id} wizard={wizard} getDeletedWizard={props.getDeletedWizard}/>
+  })
+
   return (
     <section>
       <h2>Students of Hogwarts</h2>
       <ul className="cards">
-        {/* Render Wizards Here*/}
+        {wizards}
       </ul>
     </section>
   )
--- src/components/Hogwarts.jsx
+++ src/components/Hogwarts.jsx
@@ -5,12 +5,82 @@ import MaraudersMap from './MaraudersMap'
 
 class Hogwarts extends Component {
 
+  state = {
+    wizards: [],
+    selectedHouse: "All"
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:4000/wizards")
+    .then(resp => resp.json())
+    .then(arrayOfWizards => {
+      this.setState({
+        wizards: arrayOfWizards
+      })
+    })
+  }
+
+  addWizardToState = (newWizard) => {
+    let copyOfWizards = [...this.state.wizards, newWizard]
+    this.setState({
+      wizards: copyOfWizards
+    })
+  }
+
+  // grabs info from child and saves to state
+  filterWizards = (optionWeGetBack) => {
+    this.setState({
+      selectedHouse: optionWeGetBack
+    })
+  }
+
+  // takes state and filters based on it
+  helpFilterWizards = () => {
+    if (this.state.selectedHouse === "All") {
+      return this.state.wizards
+    } else {
+      return this.state.wizards.filter((wizard) => {
+        return wizard.house.includes(this.state.selectedHouse)
+      })
+    } 
+    // let {selectedHouse} = this.state
+    // switch (selectedHouse) {
+    //   case "All":
+    //     return this.state.wizards
+    //   case "Gryffindor":
+    //     return this.state.wizards.filter((wizard) => {
+    //           return wizard.house === "Gryffindor"
+    //     })
+    //     case "Gryffindor":
+    //       return this.state.wizards.filter((wizard) => {
+    //             return wizard.house === "Gryffindor"
+    //       })
+    // }
+  }
+
+  getDeletedWizard = (id) => {
+    // const copyOfWizards = [...this.state.wizards]
+    const filteredWizard = this.state.wizards.filter((wizard) => {
+      return wizard.id !== id 
+    })
+    this.setState({
+      wizards: filteredWizard
+    })
+  }
+
   render() {
+    // let filteredArray = this.state.wizards.filter((wizard) => {
+    //   if (this.state.selectedHouse === "All") {
+    //     return this.state.wizards
+    //   } else if (wizard.house === this.state.selectedHouse) {
+    //     return wizard
+    //   }
+    // })
     return (
       <main>
-        <MaraudersMap/>
-        <GreatHall/>
-        <SortingHat/>
+        <MaraudersMap filterWizards={this.filterWizards} selectedHouse={this.state.selectedHouse}/>
+        <GreatHall wizards={this.helpFilterWizards()} getDeletedWizard={this.getDeletedWizard}/>
+        <SortingHat addWizard={this.addWizardToState}/>
       </main>
     )
   }
